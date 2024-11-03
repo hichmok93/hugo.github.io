f238f3169a4e9a9e57f25b1a6cbe8bfc27fb56bd
hichmok93 <hi.mokhtari@live.com>
2024-11-03 22:21:48 +0100

    .

--- exampleSite/pull-images.sh
+++ exampleSite/pull-images.sh
@@ -7,15 +7,13 @@ wget --no-clobber --content-disposition --directory-prefix=content "https://unsp
 wget --no-clobber --content-disposition --directory-prefix=content/animals "https://unsplash.com/photos/Zxq_dEn_HKA/download?&force=true&w=1920"
 
 # Animals/Cats
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/Zxq_dEn_HKA/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/75715CVEJhI/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/mJaD10XeD7w/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/CEx86maLUSc/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/yMSecCHsIBc/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/rW-I87aPY5Y/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/p6yH8VmGqxo/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/LEpfefQf4rU/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/nKC772R_qog/download?&force=true&w=1920"
+wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/9met83Bgmvg/download?force=true&w=1920"
+wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/0YddxxLxLds/download?force=true&w=1920"
+wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/OXLuw_oYeVM/download?force=true&w=1920"
+wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/Y4dcKc8UJqI/download?force=true&w=1920"
+wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/uHl9FthpMm4/download?force=true&w=1920"
+wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/esFif-IwjOs/download?force=true&w=1920"
+
 
 exiftool -ImageDescription="Brown tabby cat on white stairs by Alexander London" content/animals/cats/alexander-london-mJaD10XeD7w-unsplash.jpg
 exiftool -ImageDescription="selective focus photography of orange and white cat on brown table by Amber Kipp" content/animals/cats/amber-kipp-75715CVEJhI-unsplash.jpg
